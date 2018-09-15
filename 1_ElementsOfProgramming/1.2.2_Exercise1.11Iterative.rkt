5d5cb7427b8cc5032f6d4f5b26ee32ece3207a19
Virjanand <virjanand@virjanand.com>
2018-09-15 06:01:11 +0200

    Finished exercise 1.11: iterative fibonacci, start ex 1.12

--- 1_ElementsOfProgramming/1.2.2_Exercise1.11Iterative.rkt
+++ 1_ElementsOfProgramming/1.2.2_Exercise1.11Iterative.rkt
@@ -1,7 +1,44 @@
 #lang sicp
 
 (define (f n)
-  (f-iter n 0 0 0))
-(define (f-iter n f-n-1 f-n-2 f-n-3)
-  (cond 
-          
+  (f-iter n 0 0 0 0))
+(define (f-iter n counter f-n-1 f-n-2 f-n-3)
+  (cond ((and (= counter 0)
+              (<= counter n)) (f-iter n
+                               (+ counter 1)
+                               0
+                               0
+                               counter))
+        ((and (= counter 1)
+              (<= counter n)) (f-iter n
+                               (+ counter 1)
+                               counter
+                               counter
+                               f-n-3))
+        ((and (= counter 2)
+              (<= counter n)) (f-iter n
+                               (+ counter 1)
+                               counter
+                               f-n-2
+                               f-n-3))
+        ((and (>= counter 3)
+              (<= counter n)) (f-iter n
+                               (+ counter 1)
+                               (+ f-n-1
+                                  (* 2 f-n-2)
+                                  (* 3 f-n-3))
+                               f-n-1
+                               f-n-2))
+        (else f-n-1)))
+
+(define (f2 n)
+  (f-iter2 n 0 1 2))
+(define (f-iter2 n f-n-1 f-n-2 f-n-3)
+  (if (= n 0)
+      f-n-1
+      (f-iter2 (- n 1)
+               f-n-2
+               f-n-3
+              (+ f-n-3
+                 (* 2 f-n-2)
+                 (* 3 f-n-1)))))
