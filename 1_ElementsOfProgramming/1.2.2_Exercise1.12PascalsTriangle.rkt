ebb78241c18ac669b24949dd094ec8a43116bca9
Virjanand <virjanand@virjanand.com>
2018-09-16 17:17:36 +0200

    First attempt at Pascal's triangle

--- 1_ElementsOfProgramming/1.2.2_Exercise1.12PascalsTriangle.rkt
+++ 1_ElementsOfProgramming/1.2.2_Exercise1.12PascalsTriangle.rkt
@@ -1,4 +1,12 @@
 #lang sicp
 
-(define (pascalTriangle n)
-  n)
+(define (pascalTriangle row column)
+  (if (= row 1)
+      1
+      (if (or (= column 1)
+              (= column row))
+          1
+          (+ (pascalTriangle (- row 1)
+                             (- column 1))
+             (pascalTriangle (- row 1)
+                             (+ column 1))))))
