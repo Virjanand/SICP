4aadce9a01a2686c882533eef0d3d6e283454cf0
Virjanand <virjanand@virjanand.com>
2018-09-29 06:13:19 +0200

    Read 1.2.4 and 1.2.5 on exponentiation and greatest common divisor with Euclid's theorem

--- 1_ElementsOfProgramming/1.2.2_Exercise1.12PascalsTriangle.rkt
+++ 1_ElementsOfProgramming/1.2.2_Exercise1.12PascalsTriangle.rkt
@@ -1,12 +1,10 @@
 #lang sicp
 
 (define (pascalTriangle row column)
-  (if (= row 1)
+  (if (or (= column 1)
+          (= column row))
       1
-      (if (or (= column 1)
-              (= column row))
-          1
-          (+ (pascalTriangle (- row 1)
-                             (- column 1))
-             (pascalTriangle (- row 1)
-                             (+ column 1))))))
+      (+ (pascalTriangle (- row 1)
+                         (- column 1))
+         (pascalTriangle (- row 1)
+                         column))))
