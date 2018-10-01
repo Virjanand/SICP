c4afc520be9b6848a4f2f2ba56100841814e66ef
Virjanand <virjanand@virjanand.com>
2018-10-01 13:04:25 +0200

    Procedures as arguments

---
+++ 1_ElementsOfProgramming/1.3.1_SummingDirect.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+
+(define (sum-integers a b)
+  (if (> a b)
+      0
+      (+ a (sum-integers (+ a 1) b))))
+
+(define (sum-cubes a b)
+  (if (> a b)
+      0
+      (+ (cube a) (sum-cubes (+ a 1) b))))
+
+(define (pi-sum a b)
+  (if (> a b)
+      0
+      (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
+
+(define (cube n)
+  (* n n n))
