cced35515afccb2e2c1548a83367ecaefc91ed20
Virjanand <virjanand@virjanand.com>
2018-10-18 05:51:02 +0200

    Finish chapter 1: create abstract procedures for reuse

---
+++ 1_ElementsOfProgramming/1.3.4_AverageDamping.rkt
@@ -0,0 +1,34 @@
+#lang sicp
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define (square x)
+  (* x x))
+
+(define (average x y)
+  (/ (+ x y)
+     2))
+
+((average-damp square) 10)
+
+; Square root using fixed point procedure
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+                    1.0))
+
+; Cube root with fixed point procedure and average damping
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y))))
+                    1.0))
