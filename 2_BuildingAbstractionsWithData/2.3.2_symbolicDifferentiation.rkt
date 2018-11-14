185221a1c1272c68440cd4c68ee8c3577cf42307
Virjanand <virjanand@virjanand.com>
2018-11-14 15:12:22 +0100

    Symbolic differentiation

---
+++ 2_BuildingAbstractionsWithData/2.3.2_symbolicDifferentiation.rkt
@@ -0,0 +1,65 @@
+#lang sicp
+(#%require (only racket/base
+                 print-as-expression
+                 print-pair-curly-braces
+                 print-mpair-curly-braces))
+(print-as-expression      #f)
+(print-pair-curly-braces  #t)
+(print-mpair-curly-braces #f)
+
+(define a 1)
+(define b 2)
+(list a b)
+(list 'a 'b)
+(list 'a b)
+
+(car '(a b c))
+
+(cdr '(a b c))
+
+; Check if symbol in list
+(define (memq item x)
+  (cond ((null? x) false)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))))
+(memq 'apple '(pear banana prune))
+(memq 'apple '(x (apple sauce) y apple pear))
+
+
+; Differentiation rules
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        (else
+         (error *unknown expression type -- DERIV* exp))))
+
+; Representing algebraic expressions
+(define (variable?) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum a1 a2) (list '+ a1 a2))
+(define (make-product m1 m2) (list '* m1 m2))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (addend s) (cadr s))
+(define (augend s) (caddrs s))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
---
+++ 2_BuildingAbstractionsWithData/2.3.2_symbolicDifferentiationSimplified.rkt
@@ -0,0 +1,82 @@
+#lang sicp
+(#%require (only racket/base
+                 print-as-expression
+                 print-pair-curly-braces
+                 print-mpair-curly-braces))
+(print-as-expression      #f)
+(print-pair-curly-braces  #t)
+(print-mpair-curly-braces #f)
+
+(define a 1)
+(define b 2)
+(list a b)
+(list 'a 'b)
+(list 'a b)
+
+(car '(a b c))
+
+(cdr '(a b c))
+
+; Check if symbol in list
+(define (memq item x)
+  (cond ((null? x) false)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))))
+(memq 'apple '(pear banana prune))
+(memq 'apple '(x (apple sauce) y apple pear))
+
+
+; Differentiation rules
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+; Representing algebraic expressions
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (addend s) (cadr s))
+(define (augend s) (caddr s))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+
+; Examples of behaviour
+(deriv '(+ x 3) 'x)
+(deriv '(* x y) 'x)
+(deriv '(* (* x y) (+ x 3)) 'x)
