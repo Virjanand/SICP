f4113fe7ab1c1875fbaa69b8d12fde74b97f37af
Virjanand <virjanand@virjanand.com>
2018-11-26 09:00:29 +0100

    Sets as lists

---
+++ 2_BuildingAbstractionsWithData/2.3.3_SetsAsUnorderedLists.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+(#%require (only racket/base
+                 print-as-expression
+                 print-pair-curly-braces
+                 print-mpair-curly-braces))
+(print-as-expression      #f)
+(print-pair-curly-braces  #t)
+(print-mpair-curly-braces #f)
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
